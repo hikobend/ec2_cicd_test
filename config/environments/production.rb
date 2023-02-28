76d4d6fe145b0fa23dfb6c03291c572ebddb1ac2
hikobend <hiko.bkend@gmail.com>
2023-02-28 11:20:16 +0900

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.hosts << "micro-alb-484569792.ap-northeast-1.elb.amazonaws.com"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
