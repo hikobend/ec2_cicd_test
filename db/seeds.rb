3675c9a28f9fa1689bed8122f23bf6fed2139f47
hikobend <hiko.bkend@gmail.com>
2023-03-24 16:01:19 +0900

    show

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Post.create(title: "title1")
+Post.create(title: "title2")
+Post.create(title: "title3")
