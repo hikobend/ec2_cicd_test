655bc6ff36d8ad2e73fd0ccb6be59371f2136538
hikobend <hiko.bkend@gmail.com>
2023-03-24 19:32:20 +0900

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'test/index'
+  root 'test#index'
   get 'test/:id', to: 'test#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
